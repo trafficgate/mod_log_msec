99859c54d097fc5a9102690ba32113e98b7b8b0c
Andrew Miller <andrew.miller@rakuten.com>
2016-06-13 12:22:59 +0900

    For Apache 2.0

--- mod_log_msec.c
+++ mod_log_msec.c
@@ -1,74 +1,85 @@
+//
+// log に 1970 01/01 からの msec を出すモジュール
+// apr_time_now() は GMT を返すので、1970/01/01 00:00:00 からの msec を返す
+// msec までの精度で時刻が必要な方にどうぞ
+//
+//
+// How to Use.
+// 1. Add "%M" in LogFormat line on httpd.conf.
+// 2. Add "LoadModule msec_module modules/mod_log_msec.so" on httpd.conf.
+//
+// %...M:   msec from 1970 01/01 00:00:00.
+//
+//
 #include "httpd.h"
 #include "http_config.h"
 #include "http_protocol.h"
 #include "ap_config.h"
 
-static int begin_msec_count(request_rec *r)
+#include "apr_strings.h"
+#include "mod_log_config.h"
+
+
+//
+// Convert apr_time_t to string.
+//
+// This code uses the same technique as apr_itoa();
+// See the function in srclib/apr/strings/apr_strings.c for more infomation.
+//
+static char *timetoa(apr_pool_t *p, apr_time_t n)
 {
-    struct timeval *stime;
-    int rc;
+    const int BUFFER_SIZE = sizeof(apr_time_t) * 3 + 2;
+    char *buf = apr_palloc(p, BUFFER_SIZE);
 
-    if (!ap_is_initial_req(r))
+    char *start = buf + BUFFER_SIZE - 1;
+
+    *start = 0;
+
+    do {
+        *--start = '0' + (n % 10);
+        n /= 10;
+    } while (n);
+
+    return start;
+}
+
+static const char *log_msec(request_rec *r, char *a)
+{
+#ifdef I_INSIST_ON_EXTRA_CYCLES_FOR_CLF_COMPLIANCE
+    apr_time_t request_time = apr_time_now();
+#else
+    apr_time_t request_time = r->request_time;
+#endif
+    return timetoa(r->pool, apr_time_as_msec( request_time));
+}
+
+
+static int msec_pre_config( apr_pool_t *p, apr_pool_t *plog, apr_pool_t *ptemp)
+{
+    static APR_OPTIONAL_FN_TYPE(ap_register_log_handler) *log_register;
+
+    log_register = APR_RETRIEVE_OPTIONAL_FN(ap_register_log_handler);
+    if (!log_register)
         return DECLINED;
 
-    stime = ap_pcalloc(r->pool, sizeof(struct timeval));
-    rc = gettimeofday(stime, NULL);
-    if (rc == -1) {
-        return DECLINED;
-    }
-
-    ap_table_setn(r->notes, "REQUEST_MICRO_SECONDS", (const char *)stime);
+    log_register(p, "M", log_msec, 0);
 
     return OK;
 }
 
-static int end_msec_count(request_rec *r)
+static void msec_register_hooks(apr_pool_t *p)
 {
-    struct timeval *stime, etime;
-    int rc;
-    const char *msec;
-    double start, end;
-
-    stime = (struct timeval *)ap_table_get(r->notes, "REQUEST_MICRO_SECONDS");
-    if (stime == NULL) {
-        return DECLINED;
-    }
-    rc = gettimeofday(&etime, NULL);
-    if (rc == -1) {
-        return DECLINED;
-    }
-
-    start = stime->tv_sec + stime->tv_usec * 1e-6;
-    end   = etime.tv_sec  + etime.tv_usec * 1e-6;
-
-    msec = ap_psprintf(r->pool, "%.6f", end - start);
-    while (r->next)
-        r = r->next;
-    ap_table_setn(r->subprocess_env, "D", msec);
-    return OK;
+    ap_hook_pre_config(msec_pre_config, NULL, NULL, APR_HOOK_LAST);
 }
-
 
 /* Dispatch list for API hooks */
-module MODULE_VAR_EXPORT log_msec_module = {
-    STANDARD_MODULE_STUFF, 
-    NULL,                  /* module initializer                  */
+module AP_MODULE_DECLARE_DATA log_msec_module = {
+    STANDARD20_MODULE_STUFF, 
     NULL,                  /* create per-dir    config structures */
     NULL,                  /* merge  per-dir    config structures */
     NULL,                  /* create per-server config structures */
     NULL,                  /* merge  per-server config structures */
     NULL,                  /* table of config file commands       */
-    NULL,                  /* [#8] MIME-typed-dispatched handlers */
-    NULL,                  /* [#1] URI to filename translation    */
-    NULL,                  /* [#4] validate user id from request  */
-    NULL,                  /* [#5] check if the user is ok _here_ */
-    NULL,                  /* [#3] check access by host address   */
-    NULL,                  /* [#6] determine MIME type            */
-    NULL,                  /* [#7] pre-run fixups                 */
-    end_msec_count,        /* [#9] log a transaction              */
-    NULL,                  /* [#2] header parser                  */
-    NULL,                  /* child_init                          */
-    NULL,                  /* child_exit                          */
-    begin_msec_count       /* [#0] post read-request              */
+    msec_register_hooks  /* register hooks                      */
 };
 
